aad333585ea18510be62f02e31b2df16a856830b
Kcholland117 <45026417+Kcholland117@users.noreply.github.com>
2019-05-31 23:53:48 -0500

    sphere No Errors 05/31/2019

--- sphere.h
+++ sphere.h
@@ -3,11 +3,11 @@
 
 #include "hitable.h"
 
-class sphere : public hitable {
+class sphere: public hitable {
 	public: 
 		sphere() {}
 		sphere(vec3 cen, float r) : center(cen), radius(r) {};
-		virtual bool hit(ray& r, float tmin, float tmax, hit_record& rec) const;
+		virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
 		vec3 center;
 		float radius;
 };
@@ -16,8 +16,25 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
 	vec3 oc = r.orgin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - a * c;
-	
+	float c = dot(oc, oc) - radius*radius;
+	float discriminant = b*b - a*c;
+	if (discriminant > 0) {
+		float temp = (-b - sqrt(b*b-a*c)) / a;
+		if (temp < t_max && temp > t_min) {
+			rec.t = temp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			return true;
+		}
+		temp = (-b + sqrt(b * b - a * c)) / a;
+		if (temp < t_max && temp > t_min) {
+			rec.t = temp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			return true;
+		}
+	}
+	return false;
+};
 
-}
+#endif
